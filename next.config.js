26f6f11d0bf3ac61855134908aee8a502de9b0c0
DrShpongle <evgeniy.nagalskiy@gmail.com>
2021-12-09 10:36:53 +0200

    clean up

--- next.config.js
+++ next.config.js
@@ -13,29 +13,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1440],
   },
   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-  // webpack(config, {isServer, defaultLoaders}) {
-  //   if (!isServer) {
-  //     config.resolve.fallback.fs = false
-  //   }
-  //   config.module.rules.push({
-  //     test: /\.mdx?$/,
-  //     use: [
-  //       defaultLoaders.babel,
-  //       {
-  //         loader: '@mdx-js/loader',
-  //         /** @type {import('@mdx-js/loader').Options} */
-  //         options: {
-  //           /* jsxImportSource: …, otherOptions… */
-
-  //         },
-  //       },
-  //     ],
-  //   })
-  //   return config
-  // },
 }
-
-// module.exports = withPlugins([], nextConfig)
 
 module.exports = withPlugins(
   [
